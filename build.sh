1c95d87ad65d6cbac45b884d0db4455a1de9379d
slimslenderslacks <slimslenderslacks@gmail.com>
2018-03-29 23:28:23 -0700

    write an image to disk when running build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ IMAGE=$DOCKER_REGISTRY/minikube-test-node:$1
 docker build -t $IMAGE -f docker/Dockerfile .
 echo $DOCKER_PASSWORD | docker login $DOCKER_REGISTRY -u $DOCKER_USER --password-stdin
 docker push $IMAGE
+echo $IMAGE >> image.txt
 
