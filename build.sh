3c20e4ba6a9d3bfc3c5e49e8024aa27052973327
Jim Clark <slimslenderslacks@gmail.com>
2018-03-30 11:29:45 -0700

    do not append

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ IMAGE=$DOCKER_REGISTRY/minikube-test-node:$1
 docker build -t $IMAGE -f docker/Dockerfile .
 docker login $DOCKER_REGISTRY -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push $IMAGE
-echo $IMAGE >> image.txt
+echo $IMAGE > image.txt
 
