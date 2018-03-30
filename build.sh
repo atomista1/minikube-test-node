0b906246f07a3cbcf705014e50882a796b182caa
Jim Clark <slimslenderslacks@gmail.com>
2018-03-30 10:46:34 -0700

    docker in minikube is not up to date with my local docker

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 IMAGE=$DOCKER_REGISTRY/minikube-test-node:$1
 docker build -t $IMAGE -f docker/Dockerfile .
-echo $DOCKER_PASSWORD | docker login $DOCKER_REGISTRY -u $DOCKER_USER --password-stdin
+docker login $DOCKER_REGISTRY -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push $IMAGE
 echo $IMAGE >> image.txt
 
