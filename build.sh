195ad98bf65d6985d8289e79c3b2786a9b2cea5b
slender slack <ding.dong.the.wonder.elf@gmail.com>
2018-03-30 16:49:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 IMAGE=$DOCKER_REGISTRY/minikube-test-node:$1
 docker build -t $IMAGE -f docker/Dockerfile .
-docker login $DOCKER_REGISTRY -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker push $IMAGE
 echo $IMAGE > image.txt
-
